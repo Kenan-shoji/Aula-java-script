23e814609d99f7ec5674d9713c27c91bb970cd9e
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-10 19:37:42 -0300

    primeiro-primeiro

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+// Portugol para JavaScript
+// Declaração de variáveis
+
+/* Em portugol:
+cadeia nome = "Kenan"
+cadeia sobrenome = "Shoji"
+inteiro idade = 20
+real peso = 95.0
+logico estudante = verdadeiro
+*/
+
+
+
+let nome 
+let sobrenome 
+let idade 
+let peso  
+let estudante 
+
+nome="Kenan"
+sobrenome="Shoji"
+idade=20
+peso=95
+estudante=true
+
+console.log (nome)
+console.log (sobrenome)
+console.log (idade)
+console.log (peso)
+console.log (estudante)
