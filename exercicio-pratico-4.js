3e3d74fbcc7960530b6ddde43caeaa3b86ae1c67
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-11 21:33:53 +0000

    exercicio pratico 4

---
+++ exercicio-pratico-4.js
@@ -0,0 +1,30 @@
+// 1. Crie seu perfil pessoal
+let meuNome = "Kenan";
+let minhaIdade = 20;
+let minhaCidade = "Pomerode";
+let minhaEscola = "Senai/SC - Pomerode";
+
+console.log("=== MEU PERFIL ===");
+console.log(`Nome: ${meuNome}`);
+console.log(`Idade: ${minhaIdade} anos`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log(`Escola: ${minhaEscola}`);
+
+// 2. Calculadora básica
+let numero1 = 20;
+let numero2 = 2;
+
+console.log("=== CALCULADORA ===");
+console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+console.log(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+console.log(`${numero1} * ${numero2} = ${numero1 * numero2}`);
+console.log(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+
+// 3. Conversões
+let celsius = 19;
+let fahrenheit = (celsius * 9/5) + 32;
+console.log(`${celsius}°C = ${fahrenheit}°F`);
+
+let metros = 12000;
+let quilometros = metros / 1000;
+console.log(`${metros}m = ${quilometros}km`);
