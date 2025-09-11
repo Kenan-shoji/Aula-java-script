918788b45154305258ce3b5ca9f28fc8c31de915
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-11 00:06:48 +0000

    exercicio

---
+++ exercicio-pratico-1.js
@@ -0,0 +1,8 @@
+let meuNome = "Kenan Shoji";
+let minhaIdade = 20;
+let minhaCidade = "Pomerode";
+let gostaDeProgramar = true;
+
+console.log(`Perfil: ${meuNome}, ${minhaIdade} anos!`);
+console.log(`Cidade: ${minhaCidade}`);
+console.log(`Gosta de Programar: ${gostaDeProgramar}`);
