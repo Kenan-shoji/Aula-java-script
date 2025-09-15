811e14d89197b098f8463a32700814d6f49815ea
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-15 23:49:40 +0000

    funcao

---
+++ funcao.js
@@ -0,0 +1,19 @@
+function adicao(n1, n2){
+    return n1+n2
+}
+
+function mult(n1, n2){
+    return n1*n2
+}
+
+function divisao(n1, n2){
+    return n1/n2
+}
+
+function sub(n1, n2){
+    return n1-n2
+}
+
+let resul = adicao(8, 7)
+
+console.log(`O valor é: ${resul}`)
