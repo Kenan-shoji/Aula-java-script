57c3f84cbee0010f60735a7716d6270f5cd19cf0
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-17 22:35:45 +0000

    funcao calculadora

--- arrow-funcao.js
+++ arrow-funcao.js
@@ -10,8 +10,34 @@ console.log(soma(3, 4));
 console.log(`Function arrow ${somaArrow(3, 4)}`);
 
 function ehPar(numero){
-    return numero%2 == 0
+    return numero%2 === 0
 }
 
 console.log(ehPar(5));
-console.log(`ehPar? ${ehPar(5)}`);
+
+const ehParArrow = numero => numero % 2 === 0
+
+console.log(ehParArrow(5));
+
+console.log("<---------------------- Calculadora ----------------------->")
+
+function Calculadora(n1, n2, operacao){
+    switch(operacao){
+        case '+':
+            return n1 + n2
+        case '-':
+            return n1 - n2
+        case '*':
+            return n1 * n2
+        case '/':
+            return n1 / n2
+        default:
+            return "Operação inválida"
+    }
+}
+
+console.log(Calculadora(10, 5, '+'))
+console.log(Calculadora(10, 5, '-'))
+console.log(Calculadora(10, 5, '*'))
+console.log(Calculadora(10, 5, '/'))
+console.log(Calculadora(10, 5, '%'))
