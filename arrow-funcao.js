212c224c022d82b44b00928d5943472e9e6b72df
kenanshoji <kenan_shoji@estudante.sc.senai.br>
2025-09-17 22:17:12 +0000

    funcao

--- arrow-funcao.js
+++ arrow-funcao.js
@@ -9,3 +9,9 @@ console.log(soma(3, 4));
 
 console.log(`Function arrow ${somaArrow(3, 4)}`);
 
+function ehPar(numero){
+    return numero%2 == 0
+}
+
+console.log(ehPar(5));
+console.log(`ehPar? ${ehPar(5)}`);
